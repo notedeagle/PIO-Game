21a07b07b2a6f7b07795c14ed246c1ad46cd890f
Dawid <notedeagle@gmail.com>
2019-10-29 14:11:04 +0100

    v0.4

---
+++ src/Game/Main.java
@@ -0,0 +1,31 @@
+package Game;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Random dice = new Random();
+       
+        Player player = new PlayerHuman(); // new PlayerComp();
+           //polimorfizm
+           
+        int number;
+        int guess;
+        
+        do {
+            number = dice.nextInt(6) + 1;
+            System.out.println("Wylosowano: " + number);
+            
+            guess = player.guess();
+            System.out.println(player.getName() + " odgaduje: " + guess);
+            
+            if(guess != number) {
+                System.out.println("Źle!\n");
+            }
+            
+        }
+        while(number != guess);
+        
+        System.out.println("Brawo!");
+    }
+}
